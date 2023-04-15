12b2386ac36d8c176734c9533f4dbd466c2aa477
Eugene Gershnik <gershnik@hotmail.com>
2023-04-15 14:56:45 -0700

    Adding KTS Android sample

---
+++ samples/android/kotlin-ksp-kts/settings.gradle.kts
@@ -0,0 +1,11 @@
+apply(from="../../common.gradle")
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "My Application"
+include(":app")
